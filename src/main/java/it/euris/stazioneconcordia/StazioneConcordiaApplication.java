f525417eb322ab18b040f8b7a065645aecb77349
buosiMatteo <buosi.matteo@gmail.com>
2023-10-17 16:41:57 +0200

    First Commit

---
+++ src/main/java/it/euris/stazioneconcordia/StazioneConcordiaApplication.java
@@ -0,0 +1,13 @@
+package it.euris.stazioneconcordia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StazioneConcordiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StazioneConcordiaApplication.class, args);
+	}
+
+}
