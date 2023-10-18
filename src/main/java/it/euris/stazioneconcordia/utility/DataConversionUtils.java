4e1142535988f8eaafa939165f4c87f881056fd4
cujbaVictor <cujba.victor96@gmail.com>
2023-10-18 11:34:56 +0200

    Data conversion added.

---
+++ src/main/java/it/euris/stazioneconcordia/utility/DataConversionUtils.java
@@ -0,0 +1,63 @@
+package it.euris.stazioneconcordia.utility;
+
+import it.euris.stazioneconcordia.data.enums.Priority;
+import it.euris.stazioneconcordia.data.enums.UserStatus;
+
+import java.time.LocalDateTime;
+
+public class DataConversionUtils {
+
+    private DataConversionUtils() {
+
+    }
+
+    public static String numberToString(Number value) {
+        return value == null ? null : value.toString();
+    }
+
+    public static Long stringToLong(String value) {
+        return value == null ? null : Long.parseLong(value);
+    }
+
+    public static LocalDateTime stringToLocalDateTime(String value) {
+        return value == null ? null : LocalDateTime.parse(value);
+    }
+
+    public static String localDateTimeToString(LocalDateTime value) {
+        return value == null ? null : value.toString();
+    }
+
+    public static Boolean stringToBoolean(String value) {
+        return value == null ? null : Boolean.valueOf(value);
+    }
+
+    public static String booleanToString(Boolean value) {
+        return value == null ? null : value.toString();
+    }
+
+    public static Priority stringToPriority(String value) {
+        for (Priority priorityValue : Priority.values()) {
+            if (priorityValue.name().equalsIgnoreCase(value))
+                return priorityValue;
+        }
+        return null;
+    }
+
+    public static String priorityToString(Priority value) {
+        return value == null ? null : value.name();
+    }
+
+    public static UserStatus stringToUserStatus(String value) {
+        for (UserStatus userStatusValue : UserStatus.values()) {
+            if (userStatusValue.name().equalsIgnoreCase(value))
+                return userStatusValue;
+        }
+        return null;
+    }
+
+    public static String userStatusToString(UserStatus value) {
+        return value == null ? null : value.name();
+    }
+
+
+}
