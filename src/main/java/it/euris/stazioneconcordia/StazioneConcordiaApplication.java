7b4db073eb1a856101500ce1088c79297d8035b5
buosiMatteo <buosi.matteo@gmail.com>
2023-10-19 14:28:21 +0200

    Activated Swagger

--- src/main/java/it/euris/stazioneconcordia/StazioneConcordiaApplication.java
+++ src/main/java/it/euris/stazioneconcordia/StazioneConcordiaApplication.java
@@ -1,9 +1,12 @@
 package it.euris.stazioneconcordia;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Stazione Concordia Project API", version = "1.0", description = "Stazione Concordia management"))
 public class StazioneConcordiaApplication {
 
 	public static void main(String[] args) {
