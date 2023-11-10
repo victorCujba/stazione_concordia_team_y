b82919662c706e4689fa3b8285aa2163742fae5d
Cristina Acsinte <cristtina.acsinte@gmail.com>
2023-11-10 14:29:58 +0100

    implement trelloController

---
+++ src/main/java/it/euris/stazioneconcordia/controller/trelloController/ListsTrelloController.java
@@ -0,0 +1,74 @@
+package it.euris.stazioneconcordia.controller.trelloController;
+
+import it.euris.stazioneconcordia.data.model.Lists;
+import it.euris.stazioneconcordia.data.trelloDto.ListsTrelloDto;
+import it.euris.stazioneconcordia.service.ListsService;
+import it.euris.stazioneconcordia.service.trelloService.ListsTrelloService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/v1/from-trello/lists")
+public  class ListsTrelloController {
+    private ListsService listsService;
+    private ListsTrelloService listsTrelloService;
+
+
+    public List<ListsTrelloDto> getAllListsFromBoard(String idBoard) {
+        return listsTrelloService.getListsByIdBoard(idBoard);
+    }
+    public List<ListsTrelloDto> getListsFromTrelloBoard(String idBoard) {
+        return listsTrelloService.getListsByIdBoard(idBoard);
+    }
+    public void findIfExistANewListOfDBAndPutOnTrello(){
+        List<Lists> listsOfDB =listsService.findAll();
+        listsOfDB.forEach(lists -> {
+            if(lists.getIdTrello().isEmpty()){
+                insertNewListFromDbToTrello(lists.getId()); }
+        });
+    }
+    public void listsCompareToLists(Lists newLists, Lists existingList ){
+
+        if (newLists.getDateLastActivity().isAfter(existingList.getDateLastActivity())||
+                newLists.getDateLastActivity().isEqual(existingList.getDateLastActivity() )){
+
+            newLists.setId(existingList.getId());
+            newLists.setDateLastActivity(LocalDateTime.now());
+            listsService.update(newLists);
+
+        }else{
+            updateAListFromDbToTrello(existingList.getId());
+        }
+    }
+    @PostMapping("/insert-new-list-to-trello")
+    public void insertNewListFromDbToTrello(@RequestParam Long idList) {
+        ListsTrelloDto listsTrelloDto=listsService.findById(idList).toDto().toTrelloDto();
+        Lists lists =listsService.findById(idList);
+
+        listsTrelloDto.setIdBoard(lists.getBoard().getIdTrello());
+        listsTrelloDto.setName(lists.getName());
+        listsTrelloService.createANewList(listsTrelloDto);
+    }
+    @PutMapping("/close-a-list-to-trello")
+    public void closeAListToTrello(@RequestParam Long idList) {
+        ListsTrelloDto listsTrelloDto=listsService.findById(idList).toDto().toTrelloDto();
+        listsTrelloDto.setClosed(String.valueOf(true));
+        Lists lists = listsService.findById(idList);
+
+        listsTrelloService.closeAList(lists.getIdTrello(),listsTrelloDto);
+    }
+    @PutMapping("/update-list-to-trello")
+    public void updateAListFromDbToTrello(@RequestParam Long idList) {
+        ListsTrelloDto listsTrelloDto= listsService.findById(idList).toDto().toTrelloDto();
+        Lists list =listsService.findById(idList);
+
+        listsTrelloDto.setIdBoard(list.getBoard().getIdTrello());
+
+        listsTrelloService.updateAList(list.getIdTrello(),listsTrelloDto);
+    }
+}
+
